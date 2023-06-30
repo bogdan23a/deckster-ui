1928d9c13eed2aa9ba07021ce6411a8088b22ac6
Bogdan Gherasim <9136242+bogdan23a@users.noreply.github.com>
2023-06-30 10:23:32 +0100

    move actions

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,18 @@
-import { getDecksPage } from '$lib/server/db';
+import { getDecksPage, saveDeck } from '$lib/server/db';
+import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
     return {
         decks: await getDecksPage(0)
+    }
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    save: async ({ request }) => {
+        const data = await request.formData();
+        const deck = await saveDeck(data);
+        throw redirect(303, "/deck/" + deck.id);
     }
 }
